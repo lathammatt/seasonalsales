82ff29fd71ea57a31ee1c83fef06da5c01b93699
Matt Latham <latham75@gmail.com>
2016-06-22 14:08:46 -0500

    starting setup

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -1,3 +1,22 @@
+"use strict";
 // pull from json files
 // prodcuts, department, price
-// 
+//
+
+var prods = new XMLHttpRequest();
+var sales = new XMLHttpRequest();
+
+prods.addEventListener("load", loadedFile);
+sales.addEventListener("load", );
+
+prods.open("GET", "products.json");
+prods.send();
+sales.open("GET", "categories.json");
+sales.send();
+
+var mainlist;
+function loadedFile(){
+	mainlist = JSON.parse(prods.responsetext);
+	applyDom(mainlist);
+};
+
